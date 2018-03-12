78d7e51e14d8debf1ac91d513e9c6d01e10086ee
jillwuu <jillwuu@users.noreply.github.com>
2018-03-12 11:18:55 -0400

    formatting

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,7 +3,7 @@ var request = require('request');
 var queue = require('../queue/jobQueue');
 var request = require('request');
 var urlExists = require('url-exists');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 module.exports = function(app, db) {
 	app.use(bodyParser.json());
@@ -16,8 +16,8 @@ module.exports = function(app, db) {
 	});
 	//get status from id
 	app.get('/fetch/:id', (req, res) => {
-		const id = req.params.id
-		const details = {'_id': new ObjectID(id)};
+		var id = req.params.id
+		var details = {'_id': new ObjectID(id)};
 		db.collection('urls').findOne(details, (err, item) =>{
 			if (!err && item != null){
 				//return status of job
@@ -32,17 +32,16 @@ module.exports = function(app, db) {
 		});
 	});
 
-  //create job from url
-  const collection = 
-  app.post('/url/', (req, res) => {
-	var url = 'http://' + req.body.url; 
-	//check if valid url
-	urlExists(url, function(err, exists){
-		if (!err && exists){
-			queue.createJob(url, res, db);
-		} else{
-			res.send({'error': 'not a valid website'});
-		}
-	})
-  });
+	//create job from url
+	app.post('/url/', (req, res) => {
+		var url = 'http://' + req.body.url; 
+		//check if valid url
+		urlExists(url, function(err, exists){
+			if (!err && exists){
+				queue.createJob(url, res, db);
+			} else{
+				res.send({'error': 'not a valid website'});
+			}
+		})
+	});
 };
